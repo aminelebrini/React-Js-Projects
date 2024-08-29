375e2cc5e1033a3c737ac2386868d18b2c72bb36
aminelebrini <aminelebrini4@gmail.com>
2024-08-29 18:42:27 +0100

    Modules

---
+++ modules/index.js
@@ -0,0 +1,9 @@
+// index.jsx
+const person1 = () => {
+    const name = "amine";
+    const age = 20;
+    const school = "1337";
+    return "I'm " + name + ", I'm " + age + " years old, and my school is " + school + ".";
+}
+
+export default person1;
